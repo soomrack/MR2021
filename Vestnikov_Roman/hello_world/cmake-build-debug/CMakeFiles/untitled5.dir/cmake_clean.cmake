79d35dea8773252a0887e26e02523c471ca7346a
Vestnikov_Roman <vestnikov.rn@edu.spbstu.ru>
2021-10-02 13:42:36 +0300

    2

---
+++ Vestnikov_Roman/hello_world/cmake-build-debug/CMakeFiles/untitled5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled5.dir/main.c.o"
+  "libuntitled5.dll.a"
+  "untitled5.exe"
+  "untitled5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
