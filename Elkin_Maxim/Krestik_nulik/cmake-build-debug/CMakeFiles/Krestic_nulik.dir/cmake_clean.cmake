e1367469dcec0d0064ab16f039b223617957c167
unknown <20alexchernik@gmail.com>
2021-10-04 01:07:10 +0300

    aaa

---
+++ Elkin_Maxim/Krestik_nulik/cmake-build-debug/CMakeFiles/Krestic_nulik.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Krestic_nulik.dir/main.c.o"
+  "Krestic_nulik.exe"
+  "Krestic_nulik.pdb"
+  "libKrestic_nulik.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Krestic_nulik.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
