cffd4f29cb1dfbdf535a6e6686d09b1fc6bb3c01
makselk <90328421+makselk@users.noreply.github.com>
2021-10-25 00:07:16 +0300

    minor corrections in code and optimizations

--- Elkin_Maxim/krestik_nulik_with_bot/BotXO.c
+++ Elkin_Maxim/krestik_nulik_with_bot/BotXO.c
@@ -4,36 +4,33 @@
 #include "stdlib.h"
 #include "stdio.h"
 
-bool z=false;
-char arr[8];
-int vac[8];
-int x;
+bool SwitchMove =  false;
+char field[9];
+bool IsCellVacant[9];
 
 // выбор случайной свободной клетки
 // p1 - записываемый символ
 // p2 - другой символ
 void rand_move(char p1, char p2) {
-    if (z == false) {
-        for (int e = 0; e <= 8; e++) {
-            if (arr[e] == p1 || arr[e] == p2) {
-                vac[e] = 10;
-            } else {
-                vac[e] = e;
-            }
+    int RandomCell = 0;
+    for (int e = 0; e <= 8; e++) {
+        if ((field[e] == p1) || (field[e] == p2)) {
+            IsCellVacant[e] = false;
         }
-        do {
-            srand(time(NULL));
-            x = rand() % 9;
-            z = false;
-            for (int e = 0; e <= 8; e++) {
-                if (vac[e] == x) {
-                    arr[e] = p1;
-                    z = true;
-                }
-            }
-        } while (z == false);
+        else {
+            IsCellVacant[e] = true;
+        }
+        printf("%d\n", IsCellVacant[e]);
     }
-}
+    do {
+        srand(time(NULL));
+        RandomCell = rand() % 9;
+        if (IsCellVacant[RandomCell] == true) {
+            field[RandomCell] = p1;
+            SwitchMove = true;
+        }
+    } while (SwitchMove == false);
+    }
 
 // алгоритм защиты
 // p1 - символ комбинации, подозрительной на выйгрышную (Х/О)
@@ -42,60 +39,60 @@ void rand_move(char p1, char p2) {
 void defend_attack(char p1, char p2, char p3) {
     for(int i=0; i<3; i++) {
         // проверка вертикальных строчек
-        if ((((arr[i]==arr[i+6])&&(arr[i]==p1))&&((arr[i+3]!=p1)&&(arr[i+3]!=p2)))&&(z==false)) {
-            arr[i+3]=p3;
-            z=true;
+        if ((((field[i]==field[i+6])&&(field[i]==p1))&&((field[i+3]!=p1)&&(field[i+3]!=p2)))&&(SwitchMove==false)) {
+            field[i+3]=p3;
+            SwitchMove=true;
         }
-        if ((((arr[i]==arr[i+3])&&(arr[i]==p1))&&((arr[i+6]!=p1)&&(arr[i+6]!=p2)))&&(z==false)) {
-            arr[i+6]=p3;
-            z=true;
+        if ((((field[i]==field[i+3])&&(field[i]==p1))&&((field[i+6]!=p1)&&(field[i+6]!=p2)))&&(SwitchMove==false)) {
+            field[i+6]=p3;
+            SwitchMove=true;
         }
-        if ((((arr[i+6]==arr[i+3])&&(arr[i+6]==p1))&&((arr[i]!=p1)&&(arr[i]!=p2)))&&(z==false)) {
-            arr[i]=p3;
-            z=true;
+        if ((((field[i+6]==field[i+3])&&(field[i+6]==p1))&&((field[i]!=p1)&&(field[i]!=p2)))&&(SwitchMove==false)) {
+            field[i]=p3;
+            SwitchMove=true;
         }
         // проверка горизонтальных строчек
-        if ((((arr[3*i]==arr[3*i+2])&&(arr[3*i]==p1))&&((arr[3*i+1]!=p1)&&(arr[3*i+1]!=p2)))&&(z==false)) {
-            arr[3*i+1]=p3;
-            z=true;
+        if ((((field[3*i]==field[3*i+2])&&(field[3*i]==p1))&&((field[3*i+1]!=p1)&&(field[3*i+1]!=p2)))&&(SwitchMove==false)) {
+            field[3*i+1]=p3;
+            SwitchMove=true;
         }
-        if ((((arr[3*i]==arr[3*i+1])&&(arr[3*i]==p1))&&((arr[3*i+2]!=p1)&&(arr[3*i+2]!=p2)))&&(z==false)) {
-            arr[3*i+2] =p3;
-            z = true;
+        if ((((field[3*i]==field[3*i+1])&&(field[3*i]==p1))&&((field[3*i+2]!=p1)&&(field[3*i+2]!=p2)))&&(SwitchMove==false)) {
+            field[3*i+2] =p3;
+            SwitchMove = true;
         }
-        if ((((arr[3*i+2]==arr[3*i+1])&&(arr[3*i+2]==p1))&&((arr[3*i]!=p1)&&(arr[3*i]!=p2)))&&(z==false)) {
-            arr[3*i] = p3;
-            z = true;
+        if ((((field[3*i+2]==field[3*i+1])&&(field[3*i+2]==p1))&&((field[3*i]!=p1)&&(field[3*i]!=p2)))&&(SwitchMove==false)) {
+            field[3*i] = p3;
+            SwitchMove = true;
         }
     }
     // проверка диагоналей
-    if ((((arr[0]==arr[8])&&(arr[0]==p1))&&((arr[4]!=p1)&&(arr[4]!=p2)))||
-        ((((arr[2]==arr[6])&&(arr[2]==p1))&&((arr[4]!=p1)&&(arr[4]!=p2)))&&(z==false))){
-        arr[4]=p3;
-        z=true;
+    if ((((field[0]==field[8])&&(field[0]==p1))&&((field[4]!=p1)&&(field[4]!=p2)))||
+        ((((field[2]==field[6])&&(field[2]==p1))&&((field[4]!=p1)&&(field[4]!=p2)))&&(SwitchMove==false))){
+        field[4]=p3;
+        SwitchMove=true;
     }
-    if ((((arr[0]==arr[4])&&(arr[0]==p1))&&((arr[8]!=p1)&&(arr[8]!=p2)))&&(z==false)){
-        arr[8]=p3;
-        z=true;
+    if ((((field[0]==field[4])&&(field[0]==p1))&&((field[8]!=p1)&&(field[8]!=p2)))&&(SwitchMove==false)){
+        field[8]=p3;
+        SwitchMove=true;
     }
-    if ((((arr[2]==arr[4])&&(arr[2]==p1))&&((arr[6]!=p1)&&(arr[6]!=p2)))&&(z==false)){
-        arr[6]=p3;
-        z=true;
+    if ((((field[2]==field[4])&&(field[2]==p1))&&((field[6]!=p1)&&(field[6]!=p2)))&&(SwitchMove==false)){
+        field[6]=p3;
+        SwitchMove=true;
     }
-    if ((((arr[8]==arr[4])&&(arr[8]==p1))&&((arr[0]!=p1)&&(arr[0]!=p2)))&&(z==false)){
-        arr[0]=p3;
-        z=true;
+    if ((((field[8]==field[4])&&(field[8]==p1))&&((field[0]!=p1)&&(field[0]!=p2)))&&(SwitchMove==false)){
+        field[0]=p3;
+        SwitchMove=true;
     }
-    if ((((arr[6]==arr[4])&&(arr[6]==p1))&&((arr[2]!=p1)&&(arr[2]!=p2)))&&(z==false)){
-        arr[2]=p3;
-        z=true;
+    if ((((field[6]==field[4])&&(field[6]==p1))&&((field[2]!=p1)&&(field[2]!=p2)))&&(SwitchMove==false)){
+        field[2]=p3;
+        SwitchMove=true;
     }
 }
 
 // Легкий бот
 // p1 - символ бота
 void bot_easy(char p1, char p2) {
-    z=false;
+    SwitchMove=false;
     printf("Bot's move!\n");
     rand_move(p1,p2);
 }
@@ -104,10 +101,10 @@ void bot_easy(char p1, char p2) {
 // p1 - символ бота
 // p2 - символ противника
 void bot_medium(char p1, char p2) {
-    z=false;
+    SwitchMove=false;
     printf("Bot's move!\n");
     defend_attack(p2,p1,p1);
-    if (z==false) {
+    if (SwitchMove==false) {
         rand_move(p1,p2);
     }
 }
@@ -116,13 +113,13 @@ void bot_medium(char p1, char p2) {
 // p1 - символ бота
 // p2 - символ противника
 void bot_hard(char p1, char p2) {
-    z=false;
+    SwitchMove=false;
     printf("Bot's move!\n");
     defend_attack(p1,p2,p1);
-    if (z==false) {
+    if (SwitchMove==false) {
         defend_attack(p2,p1,p1);
     }
-    if (z==false) {
+    if (SwitchMove==false) {
         rand_move(p1,p2);
     }
 }
