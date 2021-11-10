87505bf7c56a2ddfe2137d5d7730d507e3ed88c7
Vestnikov_Roman <vestnikov.rn@edu.spbstu.ru>
2021-11-10 19:35:22 +0300

    Крестики нолики GUI v1.11 - Картинки

---
+++ Vestnikov_Roman/krestiki_noliki_GTK_GUI+bot/cmake-build-debug/CMakeFiles/gtk-test.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/GIt/MR2021/Vestnikov_Roman/krestiki_noliki_GTK_GUI+bot/bot.c" "D:/GIt/MR2021/Vestnikov_Roman/krestiki_noliki_GTK_GUI+bot/cmake-build-debug/CMakeFiles/gtk-test.dir/bot.obj"
+  "D:/GIt/MR2021/Vestnikov_Roman/krestiki_noliki_GTK_GUI+bot/check_library.c" "D:/GIt/MR2021/Vestnikov_Roman/krestiki_noliki_GTK_GUI+bot/cmake-build-debug/CMakeFiles/gtk-test.dir/check_library.obj"
+  "D:/GIt/MR2021/Vestnikov_Roman/krestiki_noliki_GTK_GUI+bot/main.c" "D:/GIt/MR2021/Vestnikov_Roman/krestiki_noliki_GTK_GUI+bot/cmake-build-debug/CMakeFiles/gtk-test.dir/main.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:/msys64/mingw64/bin/../include/gtk-3.0"
+  "C:/msys64/mingw64/bin/../include/pango-1.0"
+  "C:/msys64/mingw64/bin/../include/glib-2.0"
+  "C:/msys64/mingw64/bin/../lib/glib-2.0/include"
+  "C:/msys64/mingw64/bin/../include/harfbuzz"
+  "C:/msys64/mingw64/bin/../include/freetype2"
+  "C:/msys64/mingw64/bin/../include/libpng16"
+  "C:/msys64/mingw64/bin/../include/fribidi"
+  "C:/msys64/mingw64/bin/../include/cairo"
+  "C:/msys64/mingw64/bin/../include/lzo"
+  "C:/msys64/mingw64/bin/../include/pixman-1"
+  "C:/msys64/mingw64/bin/../include/gdk-pixbuf-2.0"
+  "C:/msys64/mingw64/bin/../include/atk-1.0"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
