a4f2845571ecf0ad0f31b7fe9434d890cf1210f4
makselk <90328421+makselk@users.noreply.github.com>
2021-12-03 23:52:55 +0300

    Add files via upload

---
+++ Elkin_Maxim/Matrix/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "matrix_lib.h"
+using namespace std;
+
+//example of using matrix library
+int main() {
+    matrix A(3,3);
+    A.set_cells_cmd();
+    A.get_matrix();
+
+    matrix B(3,3);
+    B.set_cells_cmd();
+    B.get_matrix();
+    cout << "determinant of B: " << B.get_det() << endl;
+
+    matrix C = A + B;
+    cout << "sum of A and B" << endl;
+    C.get_matrix();
+
+    C = A - B;
+    cout << "result of A sub B" << endl;
+    C.get_matrix();
+
+    C = A * B;
+    cout << "multiplication of A and B" << endl;
+    C.get_matrix();
+
+    return 0;
+}
