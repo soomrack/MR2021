cad688c78234602247eccedfb743d8cc7a088369
kot0111 <kozyura0111@gmail.com>
2021-12-22 22:17:33 +0300

    Correction of mistakes

--- Koziura_Konstantin/Matrix/library.cpp
+++ Koziura_Konstantin/Matrix/library.cpp
@@ -23,7 +23,7 @@ Matrix::Matrix(int height, int width) {
     }
 }
 
-Matrix::Matrix(int height, int width, double *data) {
+Matrix::Matrix(int height, int width, double (&data)[]) {
     this->height = height;
     this -> width = width;
     this->data = (double *)malloc(height * width * sizeof(double));
@@ -40,7 +40,6 @@ Matrix::Matrix(Matrix &&m) {
     m.data = nullptr;
 }
 
-//Конструктор копирования
 Matrix::Matrix(const Matrix &m) {
     height=m.height;
     width=m.width;
@@ -48,7 +47,53 @@ Matrix::Matrix(const Matrix &m) {
     memcpy(data,  m.data,height * width * (sizeof(double)));
 }
 
-void Matrix::Print() {
+Matrix & Matrix::operator= (const Matrix &m) {
+    if (&m == this) { return *this; }
+    height = m.height;
+    width = m.width;
+    free(data);
+    data = (double *) malloc(height * width * sizeof(double));
+    memcpy(data, m.data, height * width * (sizeof(double)));
+    return *this;
+}
+
+Matrix & Matrix::operator+ (const Matrix &m){
+    if ((m.width != this->width) or (m.height != this->height)){
+        std::cout<<"Summation error\n";
+        return * new Matrix(0,0);
+    }
+
+    double arr[m.height * m.width];
+
+    for (int i = 0; i < m.height * m.width; ++i) {
+        arr[i] = this->data[i]+m.data[i];
+    }
+
+    return * new Matrix(m.height,m.width, arr);
+}
+
+Matrix & Matrix::operator* (const Matrix &m){
+    if (m.height != this->width){
+        std::cout<<"Multiplication error\n";
+        return * new Matrix(0,0);
+    }
+
+    double arr[this->height * m.width];
+
+    for (int i = 0; i < this->height; ++i) {
+        for (int j = 0; j < m.width; ++j) {
+            double Temp = 0.0;
+            for (int k = 0; k < m.height; ++k) {
+                Temp+= this->data[i*this->width+k]*m.data[m.width*k+j];
+            }
+            arr[i*width+j] = Temp;
+        }
+    }
+
+    return * new Matrix(this->height,m.width, arr);
+}
+
+void Matrix::print() {
     for (int i = 0; i < height; ++i) {
         for (int j = 0; j < width; ++j) {
             std::cout<< data[i*width+j]<<' ';
@@ -57,14 +102,18 @@ void Matrix::Print() {
     }
 }
 
-double Matrix::Trace() {
+double Matrix::trace() {
     if (this->width != this->height){
         return std::nanf("");
     }
-    double Temp=1;
+    double Temp=1.0;
     for (int i = 0; i < height; ++i) {
         Temp*= data[i*width+i];
     }
     return Temp;
+}
+
+Matrix::~Matrix() {
+    free(data);
 }
 
