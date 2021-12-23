49d2d070b2f8f26f7464e9ed52659a3a5d17a576
Alexander <chernikov.av@edu.spbstu.ru>
2021-12-23 15:24:25 +0300

    refactor(main.cpp) add declaration of testing functions

--- Chernikov_Alexandr/matrix/main.cpp
+++ Chernikov_Alexandr/matrix/main.cpp
@@ -2,7 +2,74 @@
 #include <iostream>
 #include <cmath>
 
-bool check_almost_equal(double a, double b, double max_rel_diff = pow(10, -10)){
+bool check_almost_equal(double a, double b, double max_rel_diff = pow(10, -10));
+void constructor_test(unsigned int height = 0, unsigned int width = 0, double value = 0.0);
+void constructor_test(unsigned int height, unsigned int width, MatrixType type);
+template <unsigned int lines, unsigned int cols>
+void constructor_test(double (&arr)[lines][cols]);
+void copy_constructor_test(Matrix &&test_matrix);
+void plus_test(Matrix &&matrix1, Matrix &&matrix2);
+void minus_test(Matrix &&matrix1, Matrix &&matrix2);
+void multiply_test(Matrix &&matrix1, Matrix &&matrix2);
+void trace_test(Matrix &&matrix_test, double valid_trace);
+void determinant_test(Matrix &&matrix_test, double valid_determinant);
+
+
+double test_array1[2][2] = {{1, 2},
+                            {4, 3}};
+double test_array2[2][2] = {{2, 2},
+                            {2, 2}};
+double test_array3[3][3] = {{1, 0, 0},
+                            {0, 1, 0},
+                            {0, 0, 1}};
+double test_array4[2][3] = {{1, 2, 3},
+                            {0, 1, 0}};
+double test_array5[3][3] = {{4, 2, 2},
+                            {2, 3, 1},
+                            {7, 3, 1}};
+double test_array6[4][4] = {{7, 11, 9, 10},
+                            {6, 6, 5, 5},
+                            {1, 2, 1, 2},
+                            {3, 4, 3, 4}};
+
+
+int main() {
+    constructor_test(0, 0, 0);
+    constructor_test(-2, -3, 5);
+    constructor_test(2, 2, 2);
+    constructor_test(2, 2, IDENTITY);
+    constructor_test(test_array2);
+
+    copy_constructor_test(Matrix(1, 2, 3));
+    copy_constructor_test(Matrix(3, 3, IDENTITY));
+
+    plus_test(Matrix(), Matrix());
+    plus_test(Matrix(1, 2, 3), Matrix(2, 2, 1));
+    plus_test(Matrix(4, 4, 4), Matrix(4, 4, IDENTITY));
+
+    minus_test(Matrix(), Matrix());
+    minus_test(Matrix(1, 2, 3), Matrix(2, 2, 1));
+    minus_test(Matrix(4, 4, 4), Matrix(4, 4, IDENTITY));
+
+    trace_test(Matrix(), 0);
+    trace_test(Matrix(1, 1, 1), 1);
+    trace_test(Matrix(2, 2, 2), 4);
+    trace_test(Matrix(2, 4, 4), 8);
+    trace_test(Matrix((double*) test_array1, 2, 2), 4);
+
+    determinant_test(Matrix(), 0);
+    determinant_test(Matrix(3, 3, IDENTITY), 1);
+    determinant_test(Matrix(1, 1, 1), 1);
+    determinant_test(Matrix(2, 2, 3), 0);
+    determinant_test(Matrix(2, 4, 4), std::nan("1"));
+    determinant_test(Matrix((double*) test_array1, 2, 2), -5);
+    determinant_test(Matrix((double*) test_array5, 3, 3), -20);
+    determinant_test(Matrix((double*) test_array6, 4, 4), 6);
+
+    return 0;
+}
+
+bool check_almost_equal(double a, double b, double max_rel_diff){
     double diff = std::abs(a - b);
     a = std::abs(a);
     b = std::abs(b);
@@ -14,7 +81,7 @@ bool check_almost_equal(double a, double b, double max_rel_diff = pow(10, -10)){
     return false;
 }
 
-void constructor_test(unsigned int height = 0, unsigned int width = 0, double value = 0.0){
+void constructor_test(unsigned int height, unsigned int width, double value){
     std::cout << "Initializing matrix with" << std::endl;
     std::cout << "height = " << height << std::endl;
     std::cout << "width = " << width << std::endl;
@@ -132,10 +199,21 @@ void minus_test(Matrix &&matrix1, Matrix &&matrix2){
     }
 }
 
+void multiply_test(Matrix &&matrix1, Matrix &&matrix2){
+    Matrix matrix_sub = matrix1 - matrix2;
+
+    std::cout << "Check matrix mul\n";
+    std::cout << "matrix1\n" << matrix1;
+    std::cout << "matrix2\n" << matrix2;
+    std::cout << "matrix_mul\n" << matrix_sub;
+
+    // implement!!!
+}
+
 void trace_test(Matrix &&matrix_test, double valid_trace){
     double checking_trace = matrix_test.tr();
     std::cout << "Checking trace of matrix\n" << matrix_test;
-    if (checking_trace != valid_trace){
+    if (!check_almost_equal(checking_trace, valid_trace)){
         std::cout << "ERROR!" << std::endl;
         std::cout << "computed trace: " << checking_trace << std::endl;
         std::cout << "valid trace: " << valid_trace << std::endl;
@@ -157,57 +235,3 @@ void determinant_test(Matrix &&matrix_test, double valid_determinant){
     }
 }
 
-
-double test_array1[2][2] = {{1, 2},
-                            {4, 3}};
-double test_array2[2][2] = {{2, 2},
-                            {2, 2}};
-double test_array3[3][3] = {{1, 0, 0},
-                            {0, 1, 0},
-                            {0, 0, 1}};
-double test_array4[2][3] = {{1, 2, 3},
-                            {0, 1, 0}};
-double test_array5[3][3] = {{4, 2, 2},
-                            {2, 3, 1},
-                            {7, 3, 1}};
-double test_array6[4][4] = {{7, 11, 9, 10},
-                            {6, 6, 5, 5},
-                            {1, 2, 1, 2},
-                            {3, 4, 3, 4}};
-
-
-int main() {
-    constructor_test(0, 0, 0);
-    constructor_test(-2, -3, 5);
-    constructor_test(2, 2, 2);
-    constructor_test(2, 2, IDENTITY);
-    constructor_test(test_array2);
-
-    copy_constructor_test(Matrix(1, 2, 3));
-    copy_constructor_test(Matrix(3, 3, IDENTITY));
-
-    plus_test(Matrix(), Matrix());
-    plus_test(Matrix(1, 2, 3), Matrix(2, 2, 1));
-    plus_test(Matrix(4, 4, 4), Matrix(4, 4, IDENTITY));
-
-    minus_test(Matrix(), Matrix());
-    minus_test(Matrix(1, 2, 3), Matrix(2, 2, 1));
-    minus_test(Matrix(4, 4, 4), Matrix(4, 4, IDENTITY));
-
-    trace_test(Matrix(), 0);
-    trace_test(Matrix(1, 1, 1), 1);
-    trace_test(Matrix(2, 2, 2), 4);
-    trace_test(Matrix(2, 4, 4), 8);
-    trace_test(Matrix((double*) test_array1, 2, 2), 4);
-
-    determinant_test(Matrix(), 0);
-    determinant_test(Matrix(3, 3, IDENTITY), 1);
-    determinant_test(Matrix(1, 1, 1), 1);
-    determinant_test(Matrix(2, 2, 3), 0);
-    determinant_test(Matrix(2, 4, 4), std::nan("1"));
-    determinant_test(Matrix((double*) test_array1, 2, 2), -5);
-    determinant_test(Matrix((double*) test_array5, 3, 3), -20);
-    determinant_test(Matrix((double*) test_array6, 4, 4), 6);
-
-    return 0;
-}
