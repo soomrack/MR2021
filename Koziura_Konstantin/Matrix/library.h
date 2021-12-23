c7d9d59f6d0c74b87c309df342033a3ae8c69264
kot0111 <kozyura0111@gmail.com>
2021-12-23 13:37:18 +0300

    Changing operator overloading

--- Koziura_Konstantin/Matrix/library.h
+++ Koziura_Konstantin/Matrix/library.h
@@ -8,11 +8,12 @@ private:
     double * data;
 public:
     ~Matrix();
-    Matrix();
+    Matrix();                                                    //Создание м-цы размером 1*1 со значением 0
+    Matrix(int height, int width);                               // Создание м-цы размером height*width, заполненной нулями
     Matrix(int height, int width, double (&data)[]);
-    Matrix(int height, int width);
-    Matrix(const Matrix &m);
-    Matrix(Matrix &&m);
+
+    __attribute__((unused)) Matrix(const Matrix &m);
+    Matrix(Matrix &&m) noexcept;
     Matrix & operator= (const Matrix &m);
     Matrix & operator+ (const Matrix &m);
     Matrix & operator* (const Matrix &m);
