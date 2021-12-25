1a1de4e712bd56ab0f5d8cd98840bb11b1a8c032
Andrey <33835790+KAD1605@users.noreply.github.com>
2021-12-25 12:15:36 +0300

    Minor change in Tic-Tac-Toe

--- Klinovitskij_Andrey/Tic_Tac_Toe/main.c
+++ Klinovitskij_Andrey/Tic_Tac_Toe/main.c
@@ -29,27 +29,27 @@
  */
 
 
-char square[10]={'1','2','3','4','5','6','7','8','9'}; //Field array. X or 0
+char square[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}; //Field array. X or 0
 
-void Field_3x3(){  //Create a tic-tac-toe field  \x1B - color
+void Field_3x3() {  //Create a tic-tac-toe field  \x1B - color
     system("cls"); //Clearing screen
     printf("\n\n\n\n\n\n\n");
     printf("\x1B[22;1;92;40m\t\t\t\t\t Tic Tac Toe \x1B[0;37;40m\n\n");
     printf("\x1B[22;1;96;40m\t\t\t\t\t   X VS 0   \x1B[0;37;40m\n\n\n");
     printf("\t\t\t\t\t    |   |    \n");
     printf("\t\t\t\t\t \x1B[22;30;47m   |   |   \x1B[0;37;40m \n");
-    printf("\t\t\t\t\t \x1B[22;30;47m %c | %c | %c \x1B[0;37;40m \n",square[6],square[7],square[8]);
+    printf("\t\t\t\t\t \x1B[22;30;47m %c | %c | %c \x1B[0;37;40m \n", square[6], square[7], square[8]);
     printf("\t\t\t\t\t_\x1B[22;30;47m___|___|___\x1B[0;37;40m_\n");
     printf("\t\t\t\t\t \x1B[22;30;47m   |   |   \x1B[0;37;40m \n");
-    printf("\t\t\t\t\t \x1B[22;30;47m %c | %c | %c \x1B[0;37;40m \n",square[3],square[4],square[5]);
+    printf("\t\t\t\t\t \x1B[22;30;47m %c | %c | %c \x1B[0;37;40m \n", square[3], square[4], square[5]);
     printf("\t\t\t\t\t_\x1B[22;30;47m___|___|___\x1B[0;37;40m_\n");
     printf("\t\t\t\t\t \x1B[22;30;47m   |   |   \x1B[0;37;40m \n");
-    printf("\t\t\t\t\t \x1B[22;30;47m %c | %c | %c \x1B[0;37;40m \n",square[0],square[1],square[2]);
+    printf("\t\t\t\t\t \x1B[22;30;47m %c | %c | %c \x1B[0;37;40m \n", square[0], square[1], square[2]);
     printf("\t\t\t\t\t \x1B[22;30;47m   |   |   \x1B[0;37;40m \n");
     printf("\t\t\t\t\t    |   |    \n");
 }
 
-int Check_Win(){   // All possible situations to win
+int Check_Win() {   // All possible situations to win
     // return 1 = Win
     // return 2 = Draw
     // return 0 = Game in progress
@@ -69,95 +69,91 @@ int Check_Win(){   // All possible situations to win
         return 1;
     else if (square[2] == square[4] && square[4] == square[6])
         return 1;
-    else if (square[0] != '1' && square[1] != '2' && square[2] != '3' && square[3] != '4' && square[4] != '5' && square[5] != '6' && square[6] != '7' && square[7] != '8' && square[8] != '9')
+    else if (square[0] != '1' && square[1] != '2' && square[2] != '3' && square[3] != '4' && square[4] != '5' &&
+             square[5] != '6' && square[6] != '7' && square[7] != '8' && square[8] != '9')
         return 0;
     else
         return 2;
 }
 
-void Foolproof(int Position, char Mark, int *player){ //Foolproof for mistakes
+void Foolproof(int Position, char Mark, int *player, int code) { //Foolproof for mistakes
     if (Position == 1 && square[0] == '1')
         square[0] = Mark;
-    else
-    if (Position == 2 && square[1] == '2')
+    else if (Position == 2 && square[1] == '2')
         square[1] = Mark;
-    else
-    if (Position == 3 && square[2] == '3')
+    else if (Position == 3 && square[2] == '3')
         square[2] = Mark;
-    else
-    if (Position == 4 && square[3] == '4')
+    else if (Position == 4 && square[3] == '4')
         square[3] = Mark;
-    else
-    if (Position == 5 && square[4] == '5')
+    else if (Position == 5 && square[4] == '5')
         square[4] = Mark;
-    else
-    if (Position == 6 && square[5] == '6')
+    else if (Position == 6 && square[5] == '6')
         square[5] = Mark;
-    else
-    if (Position == 7 && square[6] == '7')
+    else if (Position == 7 && square[6] == '7')
         square[6] = Mark;
-    else
-    if (Position == 8 && square[7] == '8')
+    else if (Position == 8 && square[7] == '8')
         square[7] = Mark;
-    else
-    if (Position == 9 && square[8] == '9')
+    else if (Position == 9 && square[8] == '9')
         square[8] = Mark;
-    else
-    {
+    else {
         //Motivational message!
-        printf("\n\n\t\tI believe in you! You will be able to click on the \x1B[1;31;46mCORRECT\x1B[0;37;40m button.\n\t\t\t\t Try again. Good luck!\n\n\t\t\t");
+        if ((*player) % 2 == 1)
+            printf("\n\n\t\tI believe in you! You will be able to click on the \x1B[1;31;46mCORRECT\x1B[0;37;40m button.\n\t\t\t\t Try again. Good luck!\n\n\t\t\t");
         (*player)--; //Back on our turn
-        system("pause"); //Wait for press key
     }
 }
 
-int User() //User interface
+int User(int code) //User interface
 {
-    int player=1, Win, Position; //Player num, Win/Draw/GiP, Position X or 0
+    int player = 1, Win, Position; //Player num, Win/Draw/GiP, Position X or 0
     char Mark; //X or 0
     system("cls");      //Clearing screen
-    do{
+    do {
         Field_3x3();             //Create a tic-tac-toe field
-        player=(player%2)?1:2;   //Check useful links (?:) or Elvis operator
+        player = (player % 2) ? 1 : 2;   //Check useful links (?:) or Elvis operator
         printf("\n\n\t\t\t\tPlayer %d \n\t\t\t\tEnter position on field: ", player);
-        scanf("%d",&Position);   //Input position
-        if (player%2==1) {
+        if (code == 2 && player == 2) {
+            Position = rand() % 10;
+        } else
+            scanf("%d", &Position);   //Input position
+        if (player % 2 == 1) {
             Mark = 'X';
-        }else{
+        } else {
             Mark = '0';
         }
-        Foolproof(Position, Mark, &player);//Foolproof
+        Foolproof(Position, Mark, &player, code);//Foolproof
         player++;       //Next turn
-        Win=Check_Win();//Checking win
-    } while(Win==2);    //While game in progress
+        Win = Check_Win();//Checking win
+    } while (Win == 2);    //While game in progress
     Field_3x3();        //Create a tic-tac-toe field
-    if(Win==1){         //If someone won
-        printf("\n\n\t\t\t\t\t\a\x1B[1;92;40mPlayer %d win\x1B[0;37;40m\n\t\t\t",--player); // --player for right winner \a for sound
+    if (Win == 1) {         //If someone won
+        printf("\n\n\t\t\t\t\t\a\x1B[1;92;40mPlayer %d win\x1B[0;37;40m\n\t\t\t",
+               --player); // --player for right winner \a for sound
         return 0;
-    }
-    else{               //If nobody won
+    } else {               //If nobody won
         printf("\n\n\t\t\t\t\t\aDraw!\n\t\t\t");
         return 0;
     }
 }
 
-void Menu(){ //Game menu
+void Menu() { //Game menu
     int key; //press key
-    for(int i=0;i<=8;i++){
-        square[i]=49+i; //Clearing field. P.S. ASCII '0' = 48 (!)
+    for (int i = 0; i <= 8; i++) {
+        square[i] = 49 + i; //Clearing field. P.S. ASCII '0' = 48 (!)
     }
     system("cls");//Clearing screen
     printf("1 - Play\n");
-    printf("2 - Exit\n");
+    printf("2 - Game with bot\n");
+    printf("3 - Exit\n");
     printf("\nChoose gamemode: ");
-    scanf("%d",&key); //Input choose
-    if(key==1){         //If 1 then start game
-        User();
-    }
-    else if(key==2){    //If 2 then close game
+    scanf("%d", &key); //Input choose
+    if (key == 1) {         //If 1 then start game
+        User(1);
+    } else if (key == 2) {    //If 2 then close game
+        User(2);
+    } else if (key == 3) {    //If 2 then close game
         exit(0);
-    }
-    else{       //If missclick
+    } else {       //If missclick
         //Motivational message!
         printf("\n\n\t\tI believe in you! You will be able to click on the \x1B[1;31;46mCORRECT\x1B[0;37;40m button.\n\t\t\t\t Try again. Good luck!\n\n\t\t\t"); // \x1B - color. Check https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
         system("pause");
@@ -165,7 +161,7 @@ void Menu(){ //Game menu
     }
 }
 
-int main(){
+int main() {
     while (1) {                     //Infinity loop
         Menu();                     //Start menu
         system("pause");  //Wait for key
