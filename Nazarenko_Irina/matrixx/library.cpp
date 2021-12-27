b426bde5ec03c575bc43103db91cb4ac79b9b929
nazAAArenko <90351387+nazAAArenko@users.noreply.github.com>
2021-12-27 12:03:16 +0300

    Add files via upload

--- Nazarenko_Irina/matrixx/library.cpp
+++ Nazarenko_Irina/matrixx/library.cpp
@@ -2,42 +2,46 @@
 #include <iostream>
 
 
-Matrix::Matrix (int height, int width) {
+Matrix::Matrix (unsigned int height, unsigned int width, double value) {
     this->height = height;
     this->width = width;
     data = (double *) malloc(height * height * sizeof(double));
-    for (int i = 0; i < height*width; i++) {
-        data[i] = 0;
-    }
+    if (data == nullptr){
+        this->height = 0;
+        this->width = 0;
+    } else for (int i = 0; i < height*width; i++) {
+            data[i] = value;
+        }
+
 }
 
-Matrix::Matrix (int height, int width, double *data) {
+
+Matrix::Matrix (unsigned int height, unsigned int width, double *data) {
     this->height = height;
     this->width = width;
     this->data = (double *) malloc(height * width * sizeof(double));
-    for (int i = 0; i < height*width; i++) {
-        this->data[i]=data[i];
-    }
+    if (data == nullptr){
+        this->height = 0;
+        this->width = 0;
+    } else for (int i = 0; i < height*width; i++) {
+            memcpy(this->data,data, height*width*sizeof(double));
+        };
 }
 
-Matrix::Matrix (int height) {
-    this->height = height;
-    width=height;
-    data = (double *) malloc(height * height * sizeof(double));
-    for (int i = 1; i <= height; i++) {
-        for (int j = 1; j <= height; j++) {
-            if (i == j)
-                data[j + (i - 1) * height - 1] = 1;
-            else data[j + (i - 1) * height - 1] = 0;
-        }
-    }
-}
 
 Matrix::Matrix(const Matrix &m){
     height=m.height;
     width=m.width;
     data=(double *)malloc(height*width*sizeof(double));
     memcpy(data, m.data, height*width*sizeof(double));
+}
+
+Matrix::Matrix(Matrix &&m)   {
+    height = m.height;
+    width = m.width;
+    free(data);
+    data = m.data;
+    m.data = nullptr;
 }
 
 Matrix & Matrix::operator= (const Matrix &m) {
@@ -51,36 +55,52 @@ Matrix & Matrix::operator= (const Matrix &m) {
     return *this;
 }
 
-Matrix & Matrix::operator + (const Matrix &m) {
-    if (this->width !=m.height)
-        std::cout << "Matrix can not be multiplied" << std::endl;
-    Matrix Plus(this->height,this->width);
-    for (int i = 1; i <=this->height; i++) {
-        for (int j = 1; j <= this->width; j++) {
-            Plus.data[j + (i - 1) * m.width - 1] = m.data[j + (i - 1) * m.width - 1]+this->data[j + (i - 1) * m.width - 1];
-        }
+Matrix Matrix::operator + (const Matrix &m) {
+    int a;
+    if ((height !=m.height)||(width!=m.width)) {
+        std::cout << "Matrix can not be summered" << std::endl;
+
     }
-    Plus.print();
-    return Plus;
+    Matrix result(height,width);
+    result.print();
+    for (int i = 0; i <height*width; i++) {
+        result.data[i] = m.data[i]+data[i];
+    };
+    return result;
 }
+
 
 void Matrix::print() {
     for (int i = 1; i <= height; i++) {
         for (int j = 1; j <= width; j++)
             std::cout << data[j + ((i - 1) * width)-1] << ' ';
         std::cout << std::endl;
+    };
+}
+
+void Matrix::Matrix_1() {
+    if (width = height) {
+        data = (double *) malloc(height * height * sizeof(double));
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= height; j++) {
+                if (i == j)
+                    data[j + (i - 1) * height - 1] = 1;
+                else data[j + (i - 1) * height - 1] = 0;
+            };
+        };
     }
+    else std::cout << "Matrix can't be made"<<std::endl;
 }
 
 void Matrix::trace() {
     int trace;
-    trace=1;
+    trace=0;
     for (int i = 1; i <= height; i++) {
         for (int j = 1; j <= height; j++) {
             if (i == j)
-                trace=trace*data[j + (i - 1) * width-1];
-        }
-    }
+                trace=trace+data[j + (i - 1) * width-1];
+        };
+    };
     std::cout <<trace<<std::endl;
 }
 
@@ -93,9 +113,9 @@ void Matrix::determinant(){
                 break;
             case 3:   std::cout << data[0]*data[4]*data[8]+ data[1]*data[5]*data[6]+data[2]*data[3]*data[7]-(data[2]*data[4]*data[6]+ data[0]*data[7]*data[5]+data[1]*data[3]*data[8]) <<std::endl;
                 break;
-            case 4:   std::cout << "This program can't calculate determinant for matrix with height>3" <<std::endl;
+            default:   std::cout << "This program can't calculate determinant for matrix with height>3" <<std::endl;
                 break;
-        }
+        };
 
     }
     else
--- Nazarenko_Irina/matrixx/library.h
+++ Nazarenko_Irina/matrixx/library.h
@@ -9,17 +9,18 @@ private:
 public:
     ~Matrix();
     Matrix();
-    Matrix(int height);
-    Matrix(int height, int width);
-    Matrix(int height, int width, double *data);
+    Matrix(unsigned int height, unsigned int width, double value=0.0);
+    Matrix(unsigned int height, unsigned int width, double *data);
     Matrix(const Matrix &m);
     Matrix(Matrix &&m);
     Matrix & operator= (const Matrix &m);
-    Matrix & operator+ (const Matrix &m);
+    Matrix operator+ (const Matrix &m);
     Matrix & operator* (const Matrix &m);
     void print();
     void determinant();
     void trace();
+    void Matrix_1(); // генерирует единичную матрицу
 };
+
 
 #endif //MATRIX_LIBRARY_H
