5b0eacf7a447a54652a090e59a921268347362f1
Slaventolo <90356162+Slaventolo@users.noreply.github.com>
2021-12-27 18:50:24 +0300

    Add files via upload

---
+++ Tolstykh_Vyacheslav/Test/Matrices/MatrixLibryary.h
@@ -0,0 +1,28 @@
+﻿#include <string.h>
+using namespace std;
+
+class Matrix
+{
+private:
+	int width;
+	int height;
+	int* data;
+public:
+	Matrix();
+	Matrix(unsigned int width, unsigned int height);
+	Matrix(unsigned int width, unsigned int height, int* other);
+	Matrix(unsigned int width, unsigned int height, int type);
+	Matrix(const Matrix& other);
+	Matrix(Matrix&& other) noexcept;
+	void print();
+	Matrix operator +(const Matrix& other);
+	Matrix operator -(const Matrix& other);
+	Matrix operator *(const Matrix& other);
+	Matrix& operator =(const Matrix& other);
+	void set_zero();
+	void set_one();
+	int trace();
+	double minor_det(unsigned int n, unsigned int m);
+	double det();
+	~Matrix(); 
+};
---
+++ Tolstykh_Vyacheslav/Test/Matrices/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string.h>
+#include"MatrixLibryary.h"
+
+int main() //пример работы с библиотекой матриц
+{
+	int A[9] = { 1,2,3,4,5,6,7,8,9 };
+	int B[12] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+	int C[9] = { 1,1,1,1,1,1,1,1,1 };
+
+	Matrix M1(3, 3, A);
+	Matrix M2(10, 10, 1);
+	Matrix M3(3, 3, C);
+	M1.set_one();
+	M1.print();
+
+	Matrix M7 = M1 - M2;
+	
+	M1 = M3;
+	M1.print();
+	cout << M1.trace();
+
+	cout<<M1.det();
+	M2.print();
+
+	Matrix M4 = M2;
+	M4.print();
+
+	Matrix M5 = M1 + M3;
+	M5.print(); 
+	Matrix M6 = M1 * M3;
+	M6.print();
+}
