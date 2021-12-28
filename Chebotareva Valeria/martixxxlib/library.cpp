1bfb268b0c2565263912c1e662460f213a0aba1d
Valerbak <90333930+Valerbak@users.noreply.github.com>
2021-12-28 16:47:29 +0300

    Add files via upload

--- Chebotareva Valeria/martixxxlib/library.cpp
+++ Chebotareva Valeria/martixxxlib/library.cpp
@@ -1,7 +1,12 @@
 #include "library.h"
 #include <iostream>
 
-//конструктор единичной мцы
+Matrix::Matrix() {
+    column = 0;
+    row = 0;
+    data = nullptr;
+}
+
 Matrix::Matrix(unsigned int column) {
     this->column = column;
     row = column;
@@ -11,7 +16,6 @@ Matrix::Matrix(unsigned int column) {
         data[i] = 1;
 }
 
-//конструктор для заполнения матрицы одним значением/нулевой мцы
 Matrix::Matrix(unsigned int column, unsigned int row, double value) {
     this->column = column;
     this->row = row;
@@ -22,16 +26,17 @@ Matrix::Matrix(unsigned int column, unsigned int row, double value) {
 }
 
 // конструктор копирования
-Matrix::Matrix(const Matrix &m) {
-    row = m.row;
-    column = m.column;
-    delete(data);
-    data = (double *) malloc(row * column * sizeof(double));
-    memcpy(this->data, m.data, row * column * sizeof(double));
+Matrix::Matrix(const Matrix &m){
+    row=m.row;
+    column=m.column;
+    free(data);
+    data = new double (row * column);
+    memcpy(this->data, m.data, row * column * sizeof(double ));
 }
 
 //конструктор перемещения
-Matrix::Matrix(Matrix &&m) noexcept {
+Matrix::Matrix(Matrix&& m) noexcept
+{
     row = m.row;
     column = m.column;
     data = m.data;
@@ -40,60 +45,87 @@ Matrix::Matrix(Matrix &&m) noexcept {
     m.data = nullptr;
 }
 
-//перегрузка операторов
 Matrix Matrix::operator*(double scalar) {
     Matrix Result = *this;
     for (int i = 0; i < row * column; i++) {
-        Result.data[i] = data[i] * scalar;
+        Result.data[i] *= scalar;
     }
     return Result;
 }
 
 Matrix Matrix::operator*(const Matrix &other) {
-    if ((column) == (other.row)) {
-        Matrix Result(row, other.column, 0.0);
-        for (int i = 0; i < (row); i++) {
-            for (int j = 0; j < Result.column; ++j) {
-                for (int jj = 0; jj < Result.row; jj++) {
-                    Result.data[i * Result.column + j] = Result.data[i * Result.column + j] +
-                                                         data[(column) * i + jj] * other.data[other.column * jj + j];
-                }
+    if ((column) != (other.row)) {
+        cout<< "ошибка операции умножения матриц: число столбцов первой матрицы должно совпадать с числом строк второй матрицы!";
+        return Matrix();
+    }
+    Matrix Result(row, other.column, 0.0);
+    for (int i = 0; i < (row); i++) {
+        for (int j = 0; j < Result.column; ++j) {
+            for (int jj = 0; jj < Result.row; jj++) {
+                Result.data[i * Result.column + j] = Result.data[i * Result.column + j] +
+                                                     data[(column) * i + jj] * other.data[other.column * jj + j];
             }
         }
-        return Result;
-    } else
-        cout
-                << "ошибка операции умножения матриц: число столбцов первой матрицы должно совпадать с числом строк второй матрицы!";
+    }
+    return Result;
 }
 
 Matrix Matrix::operator+(const Matrix &other) {
-    if ((row == other.row) && (column == other.column)) {
-        Matrix Result(other.column, other.row, 0.0);
-        for (int i = 0; i < Result.row * Result.column; i++) {
-            Result.data[i] = data[i] + other.data[i];
-        }
-        return Result;
-    } else cout << "ошибка операции сложения матриц: матрицы должны быть одного размера\n" << endl;
+    if (!((row == other.row) && (column == other.column))) {
+        cout << "ошибка операции сложения матриц: матрицы должны быть одного размера\n" << endl;
+        return Matrix();
+    }
+    Matrix Result(other.column, other.row, 0.0);
+    for (int i = 0; i < Result.row * Result.column; i++) {
+        Result.data[i] = data[i] + other.data[i];
+    }
+    return Result;
 }
 
 Matrix Matrix::operator-(const Matrix &other) {
-    if ((row == other.row) && (column == other.column)) {
-        Matrix Result(other.column, other.row, 0.0);
-        for (int i = 0; i < Result.row * Result.column; i++) {
-            Result.data[i] = data[i] + other.data[i];
-        }
-        return Result;
-    } else cout << "ошибка операции вычитания матриц: матрицы должны быть одного размера\n" << endl;
+    if (!((row == other.row) && (column == other.column))) {
+        cout << "ошибка операции вычитания матриц: матрицы должны быть одного размера\n" << endl;
+        return Matrix();
+    }
+    Matrix Result(other.column, other.row, 0.0);
+    for (int i = 0; i < Result.row * Result.column; i++) {
+        Result.data[i] = data[i] + other.data[i];
+    }
+    return Result;
 }
 
-//ф-я для обнуления матрицы
+Matrix Matrix::operator = (Matrix &&other) noexcept {
+    this->row = other.row;
+    this->column = other.column;
+    this -> data = other.data;
+    other.data = nullptr;
+    other.row = 0;
+    other.column = 0;
+    return *this;
+}
+
+Matrix Matrix::operator = (const Matrix &other){
+    if (&other == this) {
+        return *this;
+    }
+    this->row = other.row;
+    this->column = other.column;
+    this->data = new double [row * column];
+    if (data != nullptr){
+        memcpy(data,other.data,sizeof(int)*row * column);
+    }else{
+        this->row = 0;
+        this->column = 0;
+    }
+    return *this;
+}
+
 void Matrix::become_null() {
     for (int i = 0; i < row * column; i++) {
         data[i] = 0.0;
     }
 }
 
-//вывод матрицы на консоль
 void Matrix::print() {
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < column; j++) {
@@ -104,16 +136,22 @@ void Matrix::print() {
     cout << "\n" << endl;
 }
 
-//след матрицы
 double Matrix::trace() {
     double trace = 0;
-    if (column == row) {
-        for (int i = 0; i < row * column; i = i + column + 1) {
-            trace = trace + data[i];
-        }
-    } else cout << "ошибка операции нахождения следа матрицы: матрица должна быть квадратной\n" << endl;
+    if (column != row) {
+        cout << "ошибка операции нахождения следа матрицы: матрица должна быть квадратной\n" << endl;
+        return 0;
+    }
+    for (int i = 0; i < row * column; i = i + column + 1) {
+        trace = trace + data[i];
+    }
     return trace;
 }
+
+void Matrix::set_matrix(unsigned int number_column, unsigned int number_row, double value) {
+    data[number_row * row + number_column] = value;
+}
+
 
 
 
--- Chebotareva Valeria/martixxxlib/library.h
+++ Chebotareva Valeria/martixxxlib/library.h
@@ -1,56 +1,28 @@
 #ifndef MARTIXXXLIB_LIBRARY_H
 #define MARTIXXXLIB_LIBRARY_H
-
 #include <iostream>
 #include <cstring>
 using namespace std;
-
 class Matrix {
 private:
     unsigned int column = 1;
     unsigned int row = 1;
-public:
     double *data;
-
-
-    Matrix() {
-        column = 0;
-        row = 0;
-        data = nullptr;
-    }
-
-    //конструктор единичной мцы
-    Matrix(unsigned int column);
-
-    //конструктор для нулевой, ну или для матрицы, чтобы заполнить одним числом все ячейки
-    Matrix(unsigned int column, unsigned int row, double value);
-
-    // конструктор копирования
-    Matrix(const Matrix &m);
-
-    //конструктор перемещения
-    Matrix(Matrix&& m) noexcept;
-
-
-    //обнуление матрицы
-    void become_null();
-
-    //для задания матрицы
-    void set_matrix(unsigned int column, unsigned int row, double value);
-
-    //ф-я для вывода ф-ии
-    void print();
-
-    //ф-я для следа матрицы
-    double trace();
-
-    //перегрузка операторов
-    Matrix operator+(const Matrix &other);
-
+public:
+    Matrix();
+    Matrix(unsigned int column);//конструктор единичной мцы
+    Matrix(unsigned int column, unsigned int row, double value);//конструктор для нулевой, ну или для матрицы, чтобы заполнить одним числом все ячейки
+    Matrix(const Matrix &m); // конструктор копирования
+    Matrix(Matrix&& m) noexcept;//конструктор перемещения
+    void become_null();//обнуление матрицы
+    void set_matrix(unsigned int column, unsigned int row, double value);//для задания матрицы
+    void print();//ф-я для вывода ф-ии
+    double trace();//ф-я для следа матрицы
+    Matrix operator+(const Matrix &other);//перегрузка операторов
     Matrix operator*(const Matrix &other);
-
     Matrix operator*(double scalar);
-
     Matrix operator-(const Matrix &other);
+    Matrix operator = (Matrix &&other) noexcept;
+    Matrix operator = (const Matrix &other);
 };
 #endif //MARTIXXXLIB_LIBRARY_H
