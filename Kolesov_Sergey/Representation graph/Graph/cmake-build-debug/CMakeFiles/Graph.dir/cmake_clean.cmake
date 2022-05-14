8d24e3cf8738124db2b8d6a527a61461bce462c6
kolesovsergey <90346408+kolesovsergey@users.noreply.github.com>
2022-05-14 12:53:17 +0300

    Способы представления графа

---
+++ Kolesov_Sergey/Representation graph/Graph/cmake-build-debug/CMakeFiles/Graph.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Graph.dir/Graph.cpp.o"
+  "CMakeFiles/Graph.dir/main.cpp.o"
+  "Graph.exe"
+  "Graph.pdb"
+  "libGraph.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Graph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
