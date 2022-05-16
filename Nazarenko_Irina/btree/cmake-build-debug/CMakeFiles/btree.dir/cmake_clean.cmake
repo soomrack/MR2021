920507f312ce7c52b4f3b24032305634ed2a8ac8
nazAAArenko <90351387+nazAAArenko@users.noreply.github.com>
2022-05-16 09:06:15 +0300

    Add files via upload

---
+++ Nazarenko_Irina/btree/cmake-build-debug/CMakeFiles/btree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/btree.dir/main.cpp.obj"
+  "btree.exe"
+  "btree.exe.manifest"
+  "btree.pdb"
+  "libbtree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/btree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
