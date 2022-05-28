32d84c6fbff3faf93570b70dc65a401cddd73132
Havry21 <89147984+Havry21@users.noreply.github.com>
2022-05-28 13:57:48 +0300

    Add files via upload

---
+++ Gavrilov_Dmitry/Красно черное дерево/redblackTree/rbtree.h
@@ -0,0 +1,61 @@
+#pragma once
+
+using namespace std;
+
+enum Color{
+    Black,
+    Red
+};
+
+struct Node {
+    int data;
+    Node* parent;
+    Node* left;
+    Node* right;
+    Color color;
+};
+
+typedef Node* NodePtr;
+
+class RedBlackTree {
+private:
+    NodePtr root;
+    NodePtr TNULL;
+
+    void initializeNULLNode(NodePtr node, NodePtr parent);
+
+    NodePtr searchTreeHelper(NodePtr node, int key);
+
+    void deleteFix(NodePtr x);
+
+    void rbTransplant(NodePtr u, NodePtr v);
+
+    void deleteNodeHelper(NodePtr node, int key);
+
+    void insertFix(NodePtr k);
+
+    void printHelper(NodePtr root, string indent, bool last) ;
+
+    void leftRotate(NodePtr x);
+
+    void rightRotate(NodePtr x);
+
+public:
+    RedBlackTree();
+
+    NodePtr getTNULL() { return TNULL; }
+
+    NodePtr minimum(NodePtr node);
+
+    void deleteAll(){ root = TNULL; }
+
+    NodePtr searchTree(int k) { return searchTreeHelper(this->root, k); }
+
+    void insert(int key);
+
+    NodePtr getRoot() { return this->root; }
+
+    void deleteNode(int data) { deleteNodeHelper(this->root, data); }
+
+    void printTree();
+};
