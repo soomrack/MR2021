09c053854ad69921c2d8b67013915a5d9347decd
Shlykov1 <90396436+Shlykov1@users.noreply.github.com>
2022-06-02 02:03:07 +0300

    Add files via upload

---
+++ shlykov_fedor/MergeSort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+//функция, сливающая массивы
+void Merge(int *A, int first, int last)
+{
+    int middle, start, final, j;
+    int *mas=new int[100];
+    middle=(first+last)/2; //вычисление среднего элемента
+    start=first; //начало левой части
+    final=middle+1; //начало правой части
+    for(j=first; j<=last; j++) //выполнять от начала до конца
+        if ((start<=middle) && ((final>last) || (A[start]<A[final])))
+        {
+            mas[j]=A[start];
+            start++;
+        }
+        else
+        {
+            mas[j]=A[final];
+            final++;
+        }
+//возвращение результата в список
+    for (j=first; j<=last; j++) A[j]=mas[j];
+    delete[]mas;
+};
+//рекурсивная процедура сортировки
+void MergeSort(int *A, int first, int last)
+{
+    {
+        if (first<last)
+        {
+            MergeSort(A, first, (first+last)/2); //сортировка левой части
+            MergeSort(A, (first+last)/2+1, last); //сортировка правой части
+            Merge(A, first, last); //слияние двух частей
+        }
+    }
+};
+//главная функция
+int main()
+{
+     int i, n;
+    int *A=new int[100];
+    cout<<"Array size > "; cin>>n;
+    for (i=1; i<=n; i++)
+    { cout<<i<<" element > "; cin>>A[i];}
+    MergeSort(A, 1, n); //вызов сортирующей процедуры
+    cout<<"sorted array: "; //вывод упорядоченного массива
+    for (i=1; i<=n; i++) cout<<A[i]<<" ";
+    delete []A;
+    return 0;
+}
