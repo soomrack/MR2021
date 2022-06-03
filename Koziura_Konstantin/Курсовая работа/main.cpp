a5479f852406a865329a3fb6856e72f1c2f6e077
kot0111 <kozyura0111@gmail.com>
2022-06-03 11:19:07 +0300

    Jarvis with classes

--- Koziura_Konstantin/Курсовая работа/main.cpp
+++ Koziura_Konstantin/Курсовая работа/main.cpp
@@ -3,82 +3,103 @@
 #include <chrono>
 #include <random>
 
-struct point {
-    double x,y;
-    point() {
+class Point {
+    friend class Plane;
+    friend bool operator != (const Point &a, const Point &b) {
+        return !(a.x == b.x && a.y == b.y);
+    }
+private:
+    double x;
+    double y;
+public:
+    ~Point() {;}
+    Point() {
         x = 0;
         y = 0;
     }
-    point(double X, double Y) {
+    Point(double X, double Y) {
         x = X;
         y = Y;
     }
 };
 
-bool operator != (const point &a, const point &b) {
-    return !(a.x == b.x && a.y == b.y);
-}
+class Plane {
+private:
+    std::vector<Point> mas;
+    std::vector<int> convex_hull;
+    int numb;
 
-int numb;
-std::vector<point> mas;
-std::vector<int> convex_hull;
-
-std::default_random_engine generator;
-std::normal_distribution<double> distribution(0.5,0.1);
-//std::uniform_real_distribution<double> distribution(0,1);
-
-
-void input(int count) {
-    numb = count;
-    if (numb > 0) {
-        mas.resize(numb);
+    double OrientTriangle2(int p1,int p2, int p3) {
+        return mas[p1].x * (mas[p2].y - mas[p3].y) + mas[p2].x * (mas[p3].y - mas[p1].y) + mas[p3].x * (mas[p1].y - mas[p2].y);
     }
-    for (int i=0;i<numb;i++) {
-        mas[i].x = distribution(generator);
-        mas[i].y = distribution(generator);
-    }
-}
-double OrientTriangle2(const point &p1,const point &p2, const point &p3) {
-    return p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y);
-}
 
-bool isInside(const point &p1, const point &p, const point &p2) {
-    return ( p1.x <= p.x && p.x <= p2.x &&
-             p1.y <= p.y && p.y <= p2.y);
-}
-void ConvexHullJarvis(const std::vector<point> &mass, std::vector<int> &convex_hul, int count) {
+    bool isInside(int p1,int p2, int p3) {
+        return ( mas[p1].x <= mas[p2].x && mas[p2].x <= mas[p3].x &&
+                 mas[p1].y <= mas[p2].y && mas[p2].y <= mas[p3].y);
+    }
+
+public:
+
+    Plane(int numb) {
+        this->numb = numb;
+        if (numb > 0) {
+            mas.resize(numb);
+        }
+    }
+
+    ~Plane() {
+        mas.clear();
+        convex_hull.clear();
+    }
+
+    void input() {
+        std::default_random_engine generator;
+        //std::normal_distribution<double> distribution(0.5,0.1);
+        std::uniform_real_distribution<double> distribution(0,1);
+
+        for (int i=0;i<numb;i++) {
+            mas[i].x = distribution(generator);
+            mas[i].y = distribution(generator);
+        }
+    }
+
+    void ConvexHullJarvis();
+
+};
+
+void Plane::ConvexHullJarvis() {
     auto start_time = std::chrono::steady_clock::now();
     // находим самую левую из самых нижних
-    if (count < 1) {
+    if (numb < 1) {
         return;
     }
     int base = 0;
-    for (int i=1;i<count;i++) {
-        if ((mass[i].y < mass[base].y) || (mass[i].y == mass[base].y && mass[i].x < mass[base].x)) {
+    for (int i=1;i<numb;i++) {
+        if ((mas[i].y < mas[base].y) || (mas[i].y == mas[base].y && mas[i].x < mas[base].x)) {
             base = i;
         }
     }
     // эта точка точно входит в выпуклую оболочку
-    convex_hul.push_back(base);
+    convex_hull.push_back(base);
 
     int first = base;
     int cur = base;
     do {
-        int next = (cur + 1) % count;
-        for (int i=0;i<count;i++) {
-            double sign = OrientTriangle2(mass[cur], mass[next], mass[i]);
+        int next = (cur + 1) % numb;
+        for (int i=0;i<numb;i++) {
+            double sign = OrientTriangle2(cur, next, i);
             // точка mas[i] находится левее прямой ( mas[cur], mas[next] )
             if (sign < 0) // обход выпуклой оболочки против часовой стрелки
                 next = i;
                 // точка лежит на прямой, образованной точками  mas[cur], mas[next]
             else if (sign == 0) {
                 // точка mas[i] находится дальше, чем mas[next] от точки mas[cur]
-                if (isInside(mass[cur],mass[next],mass[i]))
+                if (isInside(cur,next,i))
                     next = i;
             }
         }
         cur = next;
-        convex_hul.push_back(next);
+        convex_hull.push_back(next);
     }
     while (cur != first);
     auto end_time = std::chrono::steady_clock::now();
@@ -88,10 +109,12 @@ void ConvexHullJarvis(const std::vector<point> &mass, std::vector<int> &convex_h
 
 int main() {
 
-    for (int k = 0; k < 10; ++k) {
-        input(50000);
-        ConvexHullJarvis(mas,convex_hull, numb);
+    Plane pl(250000);
+    for (int i = 0; i < 10; ++i) {
+        pl.input();
+        pl.ConvexHullJarvis();
     }
+
 
     return 0;
 }
